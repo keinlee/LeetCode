43134f503c007992f8b4d7e7f3dfdf1fd2a334b5
keinlee <zimu12u@163.com>
2017-09-09 08:32:53 +0800

    Initial commit
    Do more exercises

---
+++ Python/461 Hamming Distance.py
@@ -0,0 +1,10 @@
+﻿class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        #对两个字符串进行异或运算，并统计结果为1的个数，那么这个数就是汉明距离。
+        #bin()是字符串类型
+        return bin(x^y).count('1')
