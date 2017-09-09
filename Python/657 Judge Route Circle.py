d8508fbaee220da4bf2efb6689b67af8263b8a10
keinlee <zimu12u@163.com>
2017-09-09 10:44:59 +0800

    add 657
    Judge Route Circle

---
+++ Python/657 Judge Route Circle.py
@@ -0,0 +1,19 @@
+﻿# runtime:32ms
+class Solution(object):
+    def judgeCircle(self,moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        return moves.count('U')==moves.count('D') and moves.count('L')==moves.count('R')
+
+
+#另一种方法 runtime:33ms
+class Solution(object):
+    def judgeCircle(self,moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        c=collections.Counter(moves)
+        return c['U']==c['D'] and c['L']==c['R']
